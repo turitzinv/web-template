ed53f9c0e7d5172553e15c31be5fd391ce0c4e99
Victor Turitzin <turitzinv@gmail.com>
2024-01-07 14:26:40 -0500

    Added css to home component.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,11 +3,11 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Sample Title</h1>
+      <h1 id="home-title">Sample Title</h1>
+      <h2 id="home-slogan">Sample Slogan or Description</h2>
+      <h3 id="home-featured">Featured Items:</h3>
     </div>
   );
 };
-
-//Featured items randomized? Like picks three? 
 
 export default Home;
