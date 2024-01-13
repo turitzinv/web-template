34f3e253d0aee2f990931a65c6d646ab15c3cbc0
Victor Turitzin <turitzinv@gmail.com>
2024-01-13 13:19:14 -0500

    Added sample seeded data and ran it to help test inventory. Added index to get items in items_controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+include ActionView::Helpers::AssetUrlHelper
+puts "🌱 Seeding..."
+
+Item.create(name: "Test name", price: 1, description: "Sample description", image_url: image_path("Test_Image_1.jpeg"))
+
+puts "✅ Done seeding!"
