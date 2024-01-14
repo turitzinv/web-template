e200a467c2f527e4cb295b390f57778fb2e6751e
Victor Turitzin <turitzinv@gmail.com>
2024-01-14 16:17:07 -0500

    Added naming conventions to ItemCard.

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -4,9 +4,9 @@ const ItemCard = ({image, name, price, description}) => {
   return (
     <div id="item-card">
       <img id="item-image" src={image} alt={name} />
-      <h2>{name}</h2>
-      <p>{price}</p>
-      <p>{description}</p>
+      <h2>Title: {name}</h2>
+      <p>Price: {price}</p>
+      <p>Description:{description}</p>
     </div>
   )
 }
